c9d38c6374da08c32f9912bee29cbb6249f23d86
Tharsis Lamin <tharsisboamorte@hotmail.com>
2024-02-06 09:37:22 -0300

    Fixed Status bar and list padding

--- app/src/main/java/com/example/userlistdao/feature/view/list_presentation/List.kt
+++ app/src/main/java/com/example/userlistdao/feature/view/list_presentation/List.kt
@@ -2,9 +2,10 @@ package com.example.userlistdao.feature.view.list_presentation
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -16,24 +17,21 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
-import com.example.userlistdao.database.entities.User
 import com.example.userlistdao.feature.SortType
 import com.example.userlistdao.feature.view.add_presentation.AddContactDialog
 import com.example.userlistdao.feature.viewmodel.bloc.UserEvent
 import com.example.userlistdao.feature.viewmodel.bloc.UserState
 import com.example.userlistdao.ui.composables.ListItem
 import com.example.userlistdao.ui.composables.TopBar
+import com.example.userlistdao.ui.theme.Purple80
+import com.example.userlistdao.ui.theme.UserListDaoTheme
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalLayoutApi::class)
@@ -44,20 +42,13 @@ fun ListFragment(
 ) {
     Scaffold(
         containerColor = Color.White,
-        topBar = { Text(
-            text = "List",
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(25.dp),
-            textAlign = TextAlign.Center,
-            style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Normal),
-            color = MaterialTheme.colorScheme.onBackground
-        ) },
+        topBar = { TopBar(title = "List") },
         floatingActionButton = {
             FloatingActionButton(
                 onClick = {
                     onEvent(UserEvent.ShowDialog)
                 },
+                containerColor = Purple80,
                 modifier = Modifier
                     .padding(16.dp)
             ) {
@@ -67,24 +58,32 @@ fun ListFragment(
                 )
             }
         }
-    ) { _ ->
-        if(state.isAddingContact) {
+    ) { padding ->
+        if (state.isAddingContact) {
             AddContactDialog(state = state, onEvent = onEvent)
         }
-        Spacer(modifier = Modifier.height(25.dp))
-        LazyColumn(
-            contentPadding = PaddingValues(16.dp),
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.spacedBy(26.dp)
+        Box(
+            Modifier
+                .height(750.dp)
+                .fillMaxWidth()
         ) {
-            onEvent(UserEvent.SortUsers(SortType.ID))
-            items(state.users) { user ->
-                ListItem(
-                    id = user.id,
-                    firstName = user.firstName,
-                    lastName = user.lastName,
-                    age = user.age,
-                )
+            LazyColumn(
+                contentPadding = PaddingValues(16.dp),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(padding)
+                    .consumeWindowInsets(padding),
+                verticalArrangement = Arrangement.spacedBy(26.dp)
+            ) {
+                onEvent(UserEvent.SortUsers(SortType.ID))
+                items(state.users) { user ->
+                    ListItem(
+                        id = user.id,
+                        firstName = user.firstName,
+                        lastName = user.lastName,
+                        age = user.age,
+                    )
+                }
             }
         }
     }
@@ -93,13 +92,10 @@ fun ListFragment(
 @Composable
 @Preview
 fun ListFragmentPreview() {
-    val listOfUsers = mutableListOf<User>()
-    listOfUsers.add(
-        element = User(
-            id = 1,
-            firstName = "Tharsis",
-            lastName = "Lamin",
-            age = 24
-        )
-    )
+    UserListDaoTheme {
+        Surface {
+            ListFragment(state = UserState(), onEvent = {})
+        }
+    }
+
 }
--- app/src/main/java/com/example/userlistdao/ui/composables/TopBar.kt
+++ app/src/main/java/com/example/userlistdao/ui/composables/TopBar.kt
@@ -1,62 +1,41 @@
 package com.example.userlistdao.ui.composables
 
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.FilledIconButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
-import com.example.userlistdao.R
+import com.example.userlistdao.ui.theme.Purple80
 import com.example.userlistdao.ui.theme.UserListDaoTheme
 
 @Composable
 fun TopBar(
     title: String,
-    hasBackPressed: Boolean,
-    navController: NavController,
 ) {
     Box(
         contentAlignment = Alignment.Center,
         modifier = Modifier
-            .heightIn(56.dp)
-            .padding(horizontal = 8.dp, vertical = 16.dp)
+            .height(56.dp)
+            .background(
+                color = Purple80
+            )
     ) {
-        if(hasBackPressed){
-            FilledIconButton(
-                onClick = { navController.popBackStack() },
-                colors = IconButtonDefaults.filledIconButtonColors(
-                    containerColor = MaterialTheme.colorScheme.surface,
-                    contentColor = MaterialTheme.colorScheme.onBackground
-                ),
-                modifier = Modifier.align(Alignment.CenterStart)
-            ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.arrow_back),
-                    contentDescription = stringResource(id = R.string.back)
-                )
-            }
-        }
         Text(
             text = title,
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(20.dp),
             textAlign = TextAlign.Center,
-            style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Normal),
+            style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
             color = MaterialTheme.colorScheme.onBackground
         )
     }
@@ -64,6 +43,10 @@ fun TopBar(
 
 @Preview
 @Composable
-private fun SavesTopBarPreview() {
-
+private fun TopBarPreview() {
+    UserListDaoTheme {
+        Surface {
+            TopBar(title = "List")
+        }
+    }
 }
